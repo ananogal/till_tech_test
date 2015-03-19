96178a93e99386531eed1e7665964f88f599851d
ananogal <ana.nogal@gmail.com>
2015-03-19 14:13:50 +0000

    Setup Xcode project. Added Quick framework

---
+++ Till/Pods/Target Support Files/Pods-TillTests-Nimble/Pods-TillTests-Nimble-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TillTests_Nimble : NSObject
+@end
+@implementation PodsDummy_Pods_TillTests_Nimble
+@end
---
+++ Till/Till/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Till
+//
+//  Created by Ana Nogal on 19/03/2015.
+//  Copyright (c) 2015 Ana Nogal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
