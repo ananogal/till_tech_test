f5f02b749178c128881b48d3263331472246a7df
Ana Nogal <ananogal@Anas-MBP.home>
2015-03-24 22:46:31 +0000

    refactoring tests

--- Till/TillTests/TillViewControllerTests.swift
+++ Till/TillTests/TillViewControllerTests.swift
@@ -31,34 +31,36 @@ class TillViewControllerTests: QuickSpec {
                     btn = UIButton()
                 }
                 
-                it("creates a new order") {
-                    controller.clientNameText.text = "Ana"
-                    controller.addToBasket(btn)
-                    expect(controller.order).toNot(beNil())
+                context("creates"){
+                    it("an order") {
+                        controller.clientNameText.text = "Ana"
+                        controller.addToBasket(btn)
+                        expect(controller.order).toNot(beNil())
+                    }
+                    
+                    it("an order item") {
+                        controller.order = Order(clientName: "Ana")
+                        controller.addToBasket(btn)
+                        expect(controller.order.items.count).to(equal(1))
+                    }
                 }
                 
-                it("adds a new item to the order") {
-                    controller.order = Order(clientName: "Ana")
-                    controller.addToBasket(btn)
-                    expect(controller.order.items.count).to(equal(1))
-                }
-                
-                it("increases quantity by 1 when product is already in order") {
-                    controller.order = Order(clientName: "Ana")
-                    controller.addToBasket(btn)
-                    controller.addToBasket(btn)
-                    expect(controller.order.items.first!.quantity).to(equal(2))
-                }
-                
-                it("doesn't add a new item if already in order") {
-                    controller.order = Order(clientName: "Ana")
-                    controller.addToBasket(btn)
-                    controller.addToBasket(btn)
-                    expect(controller.order.items.count).to(equal(1)
-                    )
+                context("if the product already exists") {
+                    beforeEach() {
+                        controller.order = Order(clientName: "Ana")
+                        controller.addToBasket(btn)
+                        controller.addToBasket(btn)
+                    }
+                    
+                    it("increases quantity by 1") {
+                        expect(controller.order.items.first!.quantity).to(equal(2))
+                    }
+                    
+                    it("doesn't create an item") {
+                        expect(controller.order.items.count).to(equal(1))
+                    }
                 }
             }
-            
         }
     }
 }
